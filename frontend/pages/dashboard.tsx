676ce393f7ed829746e4ec012305285f5ffde4c7
Rafael Souza <rafael.souza@animasales.com.br>
2022-08-20 21:49:52 -0300

    initial commit

---
+++ frontend/pages/dashboard.tsx
@@ -0,0 +1,18 @@
+import { useContext, useEffect } from "react"
+import { AuthContext } from "../contexts/AuthContext"
+import { api } from "../services/api"
+
+export default function Dashboard() {
+
+  const { user } = useContext(AuthContext)
+
+  useEffect(() => {
+    api.get('/me')
+      .then(response => console.log(response))
+      .catch(err => console.log(err))
+  }, [])
+
+  return (
+    <h1>Dash: {user?.email}</h1>
+  )
+}
