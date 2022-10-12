72f4b06bba5f33909849b7c715a7b0492fc5fbe7
Rafael Souza <rafael.souza@animasales.com.br>
2022-10-12 16:24:33 -0300

    Controle de permissões - Criando componente de permissão

--- frontend/pages/dashboard.tsx
+++ frontend/pages/dashboard.tsx
@@ -7,14 +7,10 @@ import { setupAPIClient } from "../services/api";
 import { AuthContext } from "../contexts/AuthContext";
 
 import { withSSRAuth } from "../utils/withSSRAuth";
-import { useCan } from "../hooks/useCan";
+import { Can } from "../components/Can";
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
-
-  const userCanSeeMetrics = useCan({
-    permissions: ['metrics.list']
-  })
 
   useEffect(() => {
     api.get('/me')
@@ -26,7 +22,9 @@ export default function Dashboard() {
     <>
       <h1>Dashboard: {user?.email}</h1>
 
-      {userCanSeeMetrics && <div>Métricas</div>}
+      <Can permissions={['metrics.list']}>
+        <div>Métricas</div>
+      </Can>
     </>
   )
 }
