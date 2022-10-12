a59bf589f7ceb49e3824436b93b1e798fc91ba33
Rafael Souza <rafael.souza@animasales.com.br>
2022-10-12 09:29:34 -0300

    Autenticação com SSR - Validando visitante

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,6 +3,7 @@ import { FormEvent, useContext, useState } from "react";
 import { parseCookies } from 'nookies';
 
 import { AuthContext } from "../contexts/AuthContext";
+import { withSSRGuest } from "../utils/withSSRGuest";
 
 export default function Home() {
   const [email, setEmail] = useState('');
@@ -32,21 +33,10 @@ export default function Home() {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
+export const getServerSideProps = withSSRGuest(async (ctx) => {
+  return {
+    props: {
 
-  const cookies = parseCookies(ctx);
-
-  if (cookies['nextauth.token']) {
-    return {
-      redirect: {
-        destination: '/dashboard',
-        permanent: false
-      }
     }
   }
-
-
-  return {
-    props: {}
-  }
-}
+});
